79a94f324e5c6fec73417d91cb38a51720e05fec
BowTiedDeployer <bowtieddeployer@gmail.com>
2023-10-31 05:54:25 +0200

    mini dapp stfs

---
+++ client/src/constants/network.js
@@ -0,0 +1,8 @@
+export const network = 'testnet';
+// testnet, mainnet, mocknet
+
+export const serverUrl = {
+  mainnet: '',
+  testnet: 'http://localhost:3001',
+  mocknet: 'http://localhost:3001',
+};
